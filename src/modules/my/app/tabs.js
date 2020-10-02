07d0a1ddb5aa53685986fd79399a0effd15c2ec6
George Doenlen <gdoenlen@gmail.com>
2020-10-02 10:21:22 -0400

    the big format aka vertical superiority

--- src/modules/my/app/tabs.js
+++ src/modules/my/app/tabs.js
@@ -1,19 +1,19 @@
 const tabItems = {
-    components: {
-        title: 'Components',
-        value: 'components',
-        visible: false
-    },
-    lockersafejs: {
-        title: 'Locker Safe JS Resources',
-        value: 'lockersafejs',
-        visible: false
-    },
-    learning: {
-        title: 'Learning LWC + JS',
-        value: 'learning',
-        visible: false
-    }
+  components: {
+    title: 'Components',
+    value: 'components',
+    visible: false
+  },
+  lockersafejs: {
+    title: 'Locker Safe JS Resources',
+    value: 'lockersafejs',
+    visible: false
+  },
+  learning: {
+    title: 'Learning LWC + JS',
+    value: 'learning',
+    visible: false
+  }
 };
 
 const tabElements = ['components', 'lockersafejs', 'learning'];
